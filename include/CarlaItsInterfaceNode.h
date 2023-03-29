fe7970ced96eed0784a411168274f7a6d4ebac8a
Lars Ippen <lars.ippen@ika.rwth-aachen.de>
2023-03-29 16:44:12 +0200

    Remove default code

--- include/CarlaItsInterfaceNode.h
+++ include/CarlaItsInterfaceNode.h
@@ -4,8 +4,6 @@
 #include <string>
 
 #include <ros/ros.h>
-#include <std_msgs/String.h>
-
 
 namespace carla {
 
@@ -15,25 +13,11 @@ class ItsInterface {
     ItsInterface();
 
   private:
-    void timerCallback(const ros::TimerEvent& event);
-    void messageCallback(const std_msgs::String& msg);
-
-    ros::NodeHandle node_handle_;
     ros::NodeHandle private_node_handle_;
 
 
     ros::Publisher pub_;
     ros::Subscriber sub_;
-
-    ros::Timer timer_;
-
-    float parameter_float_;
-    bool parameter_bool_;
-    std::string parameter_string_;
-
-    bool create_publisher_;
-    bool create_subscriber_;
-
 };
 
 
