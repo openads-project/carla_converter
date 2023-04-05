74e85b668d2a8997ba642c8ed8e0428a2bac65d7
Lars Ippen <lars.ippen@ika.rwth-aachen.de>
2023-04-05 15:04:22 +0200

    Use new/adapted object_access setters

--- src/CarlaItsInterfaceNode.cpp
+++ src/CarlaItsInterfaceNode.cpp
@@ -22,26 +22,18 @@ void ItsInterface::objectsCallback(const derived_object_msgs::ObjectArray::Const
   for (size_t i = 0; i < msg->objects.size(); i++) {
     msg_object_list_.objects[i].id = msg->objects[i].id;
     msg_object_list_.objects[i].existence_probability = 1.0; // Probability that the object exists is always 1.0 as source is CARLA
-    perception_interfaces::ObjectAccess::initializeState(msg_object_list_.objects[i], perception_interfaces::ISCACTR::MODEL_ID);
+    perception_interfaces::object_access::initializeState(msg_object_list_.objects[i], perception_interfaces::ISCACTR::MODEL_ID);
     msg_object_list_.objects[i].state.header = msg_object_list_.header; // optional
-    perception_interfaces::ObjectAccess::setX(msg_object_list_.objects[i], msg->objects[i].pose.position.x);
-    perception_interfaces::ObjectAccess::setY(msg_object_list_.objects[i], msg->objects[i].pose.position.y);
-    perception_interfaces::ObjectAccess::setZ(msg_object_list_.objects[i], msg->objects[i].pose.position.z + msg->objects[i].shape.dimensions[2] / 2.0);
-    perception_interfaces::ObjectAccess::setVelX(msg_object_list_.objects[i], msg->objects[i].twist.linear.x);
-    perception_interfaces::ObjectAccess::setVelY(msg_object_list_.objects[i], msg->objects[i].twist.linear.y);
-    perception_interfaces::ObjectAccess::setAccX(msg_object_list_.objects[i], msg->objects[i].accel.linear.x);
-    perception_interfaces::ObjectAccess::setAccY(msg_object_list_.objects[i], msg->objects[i].accel.linear.y);
-
-    tf2::Quaternion q;
-    tf2::fromMsg(msg->objects[i].pose.orientation, q);
-    double roll, pitch, yaw;
-    tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
-    perception_interfaces::ObjectAccess::setYaw(msg_object_list_.objects[i], yaw);
-
-    perception_interfaces::ObjectAccess::setYawRate(msg_object_list_.objects[i], msg->objects[i].twist.angular.z);
-    perception_interfaces::ObjectAccess::setLength(msg_object_list_.objects[i], msg->objects[i].shape.dimensions[0]);
-    perception_interfaces::ObjectAccess::setWidth(msg_object_list_.objects[i], msg->objects[i].shape.dimensions[1]);
-    perception_interfaces::ObjectAccess::setHeight(msg_object_list_.objects[i], msg->objects[i].shape.dimensions[2]);
+    perception_interfaces::object_access::setX(msg_object_list_.objects[i], msg->objects[i].pose.position.x);
+    perception_interfaces::object_access::setY(msg_object_list_.objects[i], msg->objects[i].pose.position.y);
+    perception_interfaces::object_access::setZ(msg_object_list_.objects[i], msg->objects[i].pose.position.z + msg->objects[i].shape.dimensions[2] / 2.0);
+    perception_interfaces::object_access::setVelocity(msg_object_list_.objects[i].state, msg->objects[i].twist.linear);
+    perception_interfaces::object_access::setAcceleration(msg_object_list_.objects[i].state, msg->objects[i].accel.linear);
+    perception_interfaces::object_access::setOrientation(msg_object_list_.objects[i].state, msg->objects[i].pose.orientation);
+    perception_interfaces::object_access::setYawRate(msg_object_list_.objects[i], msg->objects[i].twist.angular.z);
+    perception_interfaces::object_access::setLength(msg_object_list_.objects[i], msg->objects[i].shape.dimensions[0]);
+    perception_interfaces::object_access::setWidth(msg_object_list_.objects[i], msg->objects[i].shape.dimensions[1]);
+    perception_interfaces::object_access::setHeight(msg_object_list_.objects[i], msg->objects[i].shape.dimensions[2]);
 
     msg_object_list_.objects[i].state.classifications.resize(1);
     msg_object_list_.objects[i].state.classifications[0].probability = 1.0; // Probability that the object is of this type is always 1.0 as source is CARLA
