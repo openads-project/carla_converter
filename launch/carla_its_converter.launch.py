73462f1fd2ef4d14c7a428605e4b23ebebd7a054
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-03-01 19:01:44 +0100

    add transform from world to relevant utm zones

--- launch/carla_its_converter.launch.py
+++ launch/carla_its_converter.launch.py
@@ -59,22 +59,22 @@ def generate_launch_description():
     transform_utm_31N = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments = ['-166021.443', '0', '0', '0', '0', '0', 'carla_map', 'utm_31N']
+        arguments = ['-166021.443', '0', '0', '0', '0', '0', 'world_origin', 'utm_31N']
     )
     transform_utm_31S = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments = ['-166021.443', '-10000000.000', '0', '0', '0', '0', 'carla_map', 'utm_31S']
+        arguments = ['-166021.443', '-10000000.000', '0', '0', '0', '0', 'world_origin', 'utm_31S']
     )
     transform_utm_30N = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments = ['-833978.557', '0', '0', '0', '0', '0', 'carla_map', 'utm_30N']
+        arguments = ['-833978.557', '0', '0', '0', '0', '0', 'world_origin', 'utm_30N']
     )
     transform_utm_30S = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments = ['-833978.557', '-10000000.000', '0', '0', '0', '0',  'carla_map', 'utm_30S']
+        arguments = ['-833978.557', '-10000000.000', '0', '0', '0', '0',  'world_origin', 'utm_30S']
     )
 
     return launch.LaunchDescription([
